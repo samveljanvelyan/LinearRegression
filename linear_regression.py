35ca9429ed7cbfd7b03cfa93665c39242711da53
samveljanvelyan <50568658+samveljanvelyan@users.noreply.github.com>
2019-05-13 11:31:25 +0400

    Add files via upload

--- linear_regression.py
+++ linear_regression.py
@@ -2,35 +2,51 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv("data.csv")
-x_axis = data['x_1'].values
-y_axis = data['y'].values
-
 
 class LinearRegression:
-    def __init__(self, x: np.ndarray = x_axis, y: np.ndarray = y_axis):
+    def __init__(self, x: np.ndarray, y: np.ndarray):
         self.x1 = x
         self.y = y
+
         if len(self.x1) != len(self.y):
             raise ValueError(print("The lengths of arrays are not equal. Please check."))
+        self.slope = None
+        self.intercept = None
 
+    def fit(self):
+        """Should calculate slope and intercept and keep on self."""
         # data count
-        self.n = len(self.x1)
+        n = len(self.x1)
 
         # Finding Σx
-        self.sum_x = sum(self.x1)
+        sum_x = sum(self.x1)
 
         # Finding Σy
-        self.sum_y = sum(self.y)
+        sum_y = sum(self.y)
 
         # Finding Σ(xy)
-        self.sum_x1_times_y = sum(self.x1 * self.y)
+        sum_x1_times_y = sum(self.x1 * self.y)
 
         # Finding Σx^2
-        self.sum_x_squared_1 = sum(self.x1 ** 2)
+        sum_x_squared_1 = sum(self.x1 ** 2)
 
         # Finding (Σx)^2
-        self.sum_x_squared_2 = self.sum_x ** 2
+        sum_x_squared_2 = sum_x ** 2
+
+        def calculate_slope():
+            numerator = (n * sum_x1_times_y) - (sum_x * sum_y)
+            denominator = n * sum_x_squared_1 - sum_x_squared_2
+            slope = numerator / denominator
+            return slope
+
+        def calculate_intercept():
+            numerator = (sum_y * sum_x_squared_1) - (sum_x * sum_x1_times_y)
+            denominator = n * sum_x_squared_1 - sum_x_squared_2
+            y_intercept = numerator / denominator
+            return y_intercept
+
+        self.slope = calculate_slope()
+        self.intercept = calculate_intercept()
 
     def draw_scatter_plot(self):
         plt.figure(figsize=(15, 10))
@@ -39,23 +55,9 @@ class LinearRegression:
         plt.title('Dependency plot')
         plt.scatter(self.x1, self.y, edgecolors='green')
 
-    def calculate_slope(self):
-        # Applying formula
-        numerator = (self.n * self.sum_x1_times_y) - (self.sum_x * self.sum_y)
-        denominator = self.n * self.sum_x_squared_1 - self.sum_x_squared_2
-        slope = numerator / denominator
-        return slope
-
-    def calculate_intercept(self):
-        # Applying formula
-        numerator = (self.sum_y * self.sum_x_squared_1) - (self.sum_x * self.sum_x1_times_y)
-        denominator = self.n * self.sum_x_squared_1 - self.sum_x_squared_2
-        y_intercept = numerator / denominator
-        return y_intercept
-
     def predict(self, x_value):
         """Given the x value predicts the y value."""
-        return self.calculate_slope() * x_value + self.calculate_intercept()
+        return self.slope * x_value + self.intercept
 
     def draw_regression_line(self):
         """Fitting the line of the model."""
@@ -79,7 +81,7 @@ class LinearRegression:
         y_part = np.array([(y - y_mean) / y_st_dev for y in self.y])
 
         numerator = sum(x_part * y_part)
-        denominator = self.n
+        denominator = len(self.x1)
 
         correlation_coefficient = numerator / denominator
         return correlation_coefficient
@@ -91,15 +93,18 @@ class LinearRegression:
         return ('Regression model is {} = {}x + {}\nCorrelation coefficient is {}\n'
                 'R-squared is {}\nDraw the regression line '
                 'using draw_regression_line function.').format('ŷ',
-                                                               round(self.calculate_slope(), 3),
-                                                               round(self.calculate_intercept(), 3),
+                                                               round(self.slope, 3),
+                                                               round(self.intercept(),
+                                                                     3),
                                                                self.correlation_coefficient(),
                                                                self.r_squared())
 
 
-model = LinearRegression()
-print(model.calculate_slope())
-print(model.calculate_intercept())
-print(model.predict(10))
-print(model.r_squared())
-model.draw_regression_line()
+if __name__ == '__main__':
+    data = pd.read_csv("/home/samvel/Desktop/Linear Regression/data.csv")
+    x_axis = data['x_1'].values
+    y_axis = data['y'].values
+
+    model = LinearRegression(x_axis, y_axis)
+    model.fit()
+    model.draw_regression_line()
